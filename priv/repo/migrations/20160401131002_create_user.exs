5c00099d9d72a33da59b57dce276c4c1344f58f0
anh <anh.trinhtrung@gmail.com>
2016-04-02 02:19:29 +1300

    initial commit

---
+++ priv/repo/migrations/20160401131002_create_user.exs
@@ -0,0 +1,15 @@
+defmodule Trello.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :first_name, :string, null: false
+      add :last_name, :string, null: false
+      add :email, :string, null: false
+      add :encrypted_password, :string, null: false
+
+      timestamps
+    end
+
+  end
+end
